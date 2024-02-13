a24ee64d027adc8014c7c4eed4b0c16029703a86
Rukshana Ramzin <Rukshana Ramzin@DESKTOP-526OJ3R>
2024-02-13 21:43:52 +0530

    added VM

--- JourneyMate/MVVM/ViewModels/BRBO/Vehicle/VehicleViewModel.cs
+++ JourneyMate/MVVM/ViewModels/BRBO/Vehicle/VehicleViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace JourneyMate.MVVM.ViewModels.BRBO.Vehicle
 {
     public partial class VehicleViewModel : BaseViewModel
     {
+        private const string ApiBaseUrl = "https://guidtourism.azurewebsites.net/api/Vehicle/";
+
+        [ObservableProperty]
+        string make;
+        [ObservableProperty]
+        string model;
+        [ObservableProperty]
+        string year;
+        [ObservableProperty]
+        string color;
+        [ObservableProperty]
+        string price;
+        [ObservableProperty]
+        string isActive;
+        [ObservableProperty]
+        string vehcicleName;
+        [ObservableProperty]
+        string vehcileNo;
+        [ObservableProperty]
+        string brand;
+        [ObservableProperty]
+        string driverName;
     }
 }

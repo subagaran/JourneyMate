f7d65525acee76efe6ac34053bb66c9e11fb72ef
Rukshana Ramzin <Rukshana Ramzin@DESKTOP-526OJ3R>
2024-02-13 21:13:22 +0530

    added view model

---
+++ JourneyMate/MVVM/ViewModels/BRBO/Vehicle/VehicleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyMate.MVVM.ViewModels.BRBO.Vehicle
+{
+    public partial class VehicleViewModel : BaseViewModel
+    {
+    }
+}

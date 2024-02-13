371fa6e86c4dc047880f483c3857da4c63a400be
Rukshana Ramzin <Rukshana Ramzin@DESKTOP-526OJ3R>
2024-02-13 22:16:25 +0530

    create vehicleVM

--- JourneyMate/MVVM/Models/VehicleModelcs.cs
+++ JourneyMate/MVVM/Models/VehicleModelcs.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace JourneyMate.MVVM.Models
 {
-    internal class VehicleModelcs
+    public class VehicleModelcs
     {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+        public string vehicleName { get; set; }
+        public string vehicleNo { get; set; }
+        public string Brand { get; set; }
+        public string driverName { get; set; }
+        public string IsActive { get; set; }
     }
 }
--- JourneyMate/MVVM/ViewModels/BRBO/Vehicle/VehicleViewModel.cs
+++ JourneyMate/MVVM/ViewModels/BRBO/Vehicle/VehicleViewModel.cs
@@ -1,14 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using JourneyMate.Database;
+using JourneyMate.MVVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace JourneyMate.MVVM.ViewModels.BRBO.Vehicle
 {
     public partial class VehicleViewModel : BaseViewModel
     {
+        private readonly HttpClient _httpClient;
+
         private const string ApiBaseUrl = "https://guidtourism.azurewebsites.net/api/Vehicle/";
 
         [ObservableProperty]
@@ -33,5 +41,54 @@ namespace JourneyMate.MVVM.ViewModels.BRBO.Vehicle
         string driverName;
 
 
+        public VehicleViewModel()
+        {
+            _httpClient = new HttpClient(); 
+        }
+
+        [RelayCommand]
+        public async Task CreateVehicle()
+        {
+            var response = await CreateHotelAsync();
+        }
+
+        public async Task<bool> CreateHotelAsync()
+        {
+            try
+            {
+                var model = new VehicleModelcs
+                {
+                    Id = 1,
+                    Make = "Toyota",
+                    Model= "Car",
+                    Year = 2023,
+                    Color = "Green",
+                    Price = 20000.00,
+                    IsActive = "Y",
+                  
+                };
+
+                var json = JsonSerializer.Serialize(model);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync(ApiBaseUrl + "CreateVehicle", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine($"Error creating while creating hotel: {ex.Message}");
+                return false; // Hotel creation failed
+            }
+        }
     }
 }
